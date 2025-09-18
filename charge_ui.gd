b25e6611478d225f2bbb962351a4d47a3e526c96
Daniel Cellucci <dwcellucci@gmail.com>
2025-09-18 08:14:13 -0700

    updated charge ui display for clarity

--- charge_ui.gd
+++ charge_ui.gd
@@ -5,16 +5,11 @@ func _ready():
 	update_display()
 	# Then connect up all of the signals with the update functions
 	$MoveSpinRow/MoveSpinBox.value_changed.connect(update_regiment_charge_range)
-	$SnapToRegimentCheckButton.toggled.connect(update_cursor_regiment_snap_setting)
+	$CycleMeasureModeButton.toggled.connect(cycle_charge_measure_mode)
 	$MeasureModeToggleButton.pressed.connect(toggle_measure_mode)
 	$CancelButton.pressed.connect(cancel_charge_mode)
 	
 func _process(delta: float):
-	# Display different text based on the barrage mode we are currently in
-	if GameSettings.charge_hud_measure_mode:
-		$MeasureModeToggleButton.text = "Switch to Overlay Mode (Tab)"
-	else: 
-		$MeasureModeToggleButton.text = "Switch to Measure Mode (Tab)"
 	process_input()
 	update_display()
 	
@@ -31,18 +26,35 @@ func process_input():
 		GameState.selected_regiment.barrage_range = GameState.selected_regiment.barrage_range + 1
 	if Input.is_action_just_pressed('UIDecreaseValue'):
 		GameState.selected_regiment.barrage_range = GameState.selected_regiment.barrage_range - 1
-
+	
+	
 func update_display():
 	# Escape clause in case selected regiment has no data
 	if GameState.selected_regiment == null:
 		return
+	# Display different text based on the barrage mode we are currently in
+	if UiStateMachine.ui_state_machine.is_charge_measure_state():
+		$MeasureModeToggleButton.text = "Switch to Overlay Mode (Tab)"
+	else: 
+		$MeasureModeToggleButton.text = "Switch to Measure Mode (Tab)"
+		
 	# The Spinbox range value should match the selected regiment's barrage range
+	# The move box should only be visible if we are in charge state but we 
+	# also aren't in a charge measure mode
+	$MoveSpinRow.visible = (UiStateMachine.ui_state_machine.is_charge_state() 
+		and not UiStateMachine.ui_state_machine.is_charge_measure_state())
 	$MoveSpinRow/MoveSpinBox.value = GameState.selected_regiment.move
+	
 	# The check button value for fluid formation should likewise match the 
 	# selected regiment
-	$SnapToRegimentCheckButton.visible = UiStateMachine.ui_state_machine.is_charge_measure_state()
-	$SnapToRegimentCheckButton.button_pressed = GameSettings.hud_regiment_snap
-	
+	$CycleMeasureModeButton.visible = UiStateMachine.ui_state_machine.is_charge_measure_state()
+	match UiStateMachine.ui_state_machine.state:
+		UiStateMachine.UIState.CHARGE_MEASURE_SNAP_FACE:
+			$CycleMeasureModeButton.text = "Cycle to free mode (space)"
+		UiStateMachine.UIState.CHARGE_MEASURE_FREE:
+			$CycleMeasureModeButton.text = "Cycle to regiment snap mode (space)"
+		UiStateMachine.UIState.CHARGE_MEASURE_SNAP_REGIMENT:
+			$CycleMeasureModeButton.text = "Cycle to face snap mode (space)"
 	$InfoText.custom_minimum_size.x = get_size().x
 	if UiStateMachine.ui_state_machine.is_charge_measure_state():
 		$InfoText.text = "Measure the distance required to charge a regiment (Press tab to swap to select target mode)"
@@ -78,6 +90,9 @@ func toggle_measure_mode():
 	if UiStateMachine.ui_state_machine.state == UiStateMachine.UIState.CHARGE_TARGET:
 		new_state = UiStateMachine.UIState.CHARGE_MEASURE_FREE
 	UiStateMachine.ui_state_machine.set_new_state(new_state)
+
+func cycle_charge_measure_mode():
+	UiStateMachine.ui_state_machine.cycle_charge_measure_state()
 
 func cancel_charge_mode():
 	UiStateMachine.ui_state_machine.cancel_state()
